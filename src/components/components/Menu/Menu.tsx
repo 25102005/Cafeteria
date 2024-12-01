d59b3270faab8b3b420cc2105dfa4e73de5f522a
e.a.guzmanmartinez <e.a.guzmanmartinez@gmail.com>
2024-11-30 23:53:09 -0600

    Final-2

--- src/components/components/Menu/Menu.tsx
+++ src/components/components/Menu/Menu.tsx
@@ -1,11 +1,14 @@
-import './Menu.styles.scss'
-const Menu = () =>{
-    return <div className="navbar-menu">
-        <a href="/desayunos">DESAYUNOS</a>
-        <a href="/bebidas">BEBIDAS</a>
-        <a href="/postres">POSTRES</a>
-        <a href="/especiales">ESPECIALES</a>
-    </div>
-}
+import './Menu.styles.scss';
 
-export {Menu};
+const Menu = () => {
+    return (
+        <div className="navbar-menu">
+            <a href="/desayunos">DESAYUNOS</a>
+            <a href="/bebidas">BEBIDAS</a>
+            <a href="/postres">POSTRES</a>
+            <a href="/especiales">ESPECIALES</a>
+        </div>
+    );
+};
+
+export { Menu };
